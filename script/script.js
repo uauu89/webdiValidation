fbf46075a2e4b92e11e0f95b67f6831b673cb4ae
uauu89 <119560529+uauu89@users.noreply.github.com>
2023-11-12 18:19:06 +0900

    book p 193

--- script/script.js
+++ script/script.js
@@ -1,26 +1,54 @@
-
-let bannerDom = document.querySelectorAll(".bannerImg"),
-    bannerNum = bannerDom.length,
-    currentIdx = 0;
-
+let slides = document.querySelectorAll(".slideItem");
+let slideIdx = 0;
 setInterval(()=>{
-    bannerDom[currentIdx].classList.remove("active");
-    currentIdx = ++currentIdx % bannerNum;
-    bannerDom[currentIdx].classList.add("active");
+    let prevIdx = slideIdx;
+    slides[prevIdx].setAttribute("class", "slideItem prev");
+
+    slideIdx = ++slideIdx % slides.length;
+    slides[slideIdx].setAttribute("class", "slideItem current");
+
+    setTimeout(()=>{
+        slides[prevIdx].classList.remove("prev")
+    }, 2000)
 }, 2500)
 
 
 
-let noticeDom = document.querySelector(".noticeItem"),
-    modalDom = document.querySelector(".modalWrap"),
-    modalCloseBtn = modalDom.querySelector(".btnClose");
 
 
-noticeDom.addEventListener("click", e=>{
-    modalDom.classList.add("active");
+
+
+
+
+
+
+let boardTap = document.querySelectorAll(".titleWrap > h2 > a"),
+    boardContents = document.querySelectorAll(".boardInner > ul");
+
+
+boardTap.forEach((title, idx)=>{
+    title.addEventListener("click", e=>{
+        boardTap.forEach(i=>i.classList.remove("active"));
+        e.currentTarget.classList.add("active");
+
+        boardContents.forEach(i=>i.classList.remove("active"));
+        boardContents[idx].classList.add("active");
+    })
 })
 
-modalCloseBtn.addEventListener("click", e=>{
+
+
+let noticeItem = document.querySelector(".notice > li:first-of-type > a");
+
+noticeItem.addEventListener("click", e=>{
     e.preventDefault();
-    modalDom.classList.remove("active");
+    document.querySelector(".modalWrap").classList.add("active");
+})
+
+let btnClose = document.querySelector(".modal .btnClose");
+
+
+btnClose.addEventListener("click", e=>{
+    e.preventDefault();
+    document.querySelector(".modalWrap").classList.remove("active");
 })
