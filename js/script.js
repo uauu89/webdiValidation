69e7c96fd317cb321d02affc7fad5726178c96e0
uauu89 <119560529+uauu89@users.noreply.github.com>
2023-11-14 22:54:25 +0900

    public C-1

--- js/script.js
+++ js/script.js
@@ -1,26 +1,44 @@
+// slide
+
 let slides = document.querySelectorAll(".slideItem"),
-    slideIdx = 0;
+    idx = 0;
 
 setInterval(()=>{
-    let prevIdx = slideIdx,
-        currentIdx = (slideIdx+1) % slides.length;
-
-    slides[prevIdx].classList.remove("current");
-    slides[currentIdx].classList.add("current");
-
-    slideIdx = currentIdx;
-}, 2800)
+    current = (idx + 1) % slides.length;
+    slides[idx].classList.remove("current");
+    slides[current].classList.add("current");
+    idx = current;
+}, 3000)
 
 
-let modalOpen = document.querySelector(".notice li:first-of-type > a"),
+// tab
+
+let tabBtn = document.querySelectorAll(".titleWrap a"),
+    boardList = document.querySelectorAll(".boardInner ul");
+
+tabBtn.forEach((btn, idx)=>{
+    btn.addEventListener("click", e=>{
+        // e.preventDefault();
+        tabBtn.forEach(i=>i.classList.remove("active"));
+        boardList.forEach(i=>i.classList.remove("active"));
+        e.currentTarget.classList.add("active");
+        boardList[idx].classList.add("active");
+    })
+})
+
+
+// modal
+
+let noticeItem = document.querySelector(".notice > li:first-of-type > a"),
     modalWrap = document.querySelector(".modalWrap"),
-    modalClose = modalWrap.querySelector(".btnClose");
+    btnClose = modalWrap.querySelector(".btnClose");
 
-modalOpen.addEventListener("click", e=>{
-    e.preventDefault();
+noticeItem.addEventListener("click", e=>{
+    // e.preventDefault();
     modalWrap.classList.add("active");
 })
-modalClose.addEventListener("click", e=>{
-    e.preventDefault();
+
+btnClose.addEventListener("click", e=>{
+    // e.preventDefault();
     modalWrap.classList.remove("active");
 })
